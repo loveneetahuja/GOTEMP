991e67930c297b7c20c2ff1be3914e8932ad63c0
Loveneet <ahujaloveneet@cosaia.com>
2022-03-29 18:54:53 +0530

    First Push

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+if (process.env.NODE_ENV == "production") {
+	//Express will serve up production assets
+	app.use(express.static("client/build"));
+
+	//Express will serve up the index.html
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "index.html"));
+	});
+
+	const PORT = process.env.PORT || 5000;
+	app.listen(PORT);
+	console.log("Glitch Over Temporary Website " + PORT);
+} else {
+	app.use(express.static("public"));
+
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "public", "index.html"));
+	});
+
+	const PORT = process.env.PORT || 5000;
+	//server.listen(PORT);
+
+	app.listen(PORT);
+	console.log("Temporary Website " + PORT);
+}
